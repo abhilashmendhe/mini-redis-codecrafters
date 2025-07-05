cbb4d333be4d872622e34044793527cf9aea2431
abhilashmendhe <abhilashmendhe@gmail.com>
2025-07-05 16:27:17 +0530

    codecrafters submit [skip ci]

--- src/errors.rs
+++ src/errors.rs
@@ -1,8 +1,10 @@
 use thiserror::Error;
 
-
 #[derive(Debug, Error)]
 pub enum RedisErrors {
     #[error("IO error: {0}")]
     Io(#[from] std::io::Error),
+
+    #[error("ParseIntError: {}", 0)]
+    ParseInt(#[from] std::num::ParseIntError)
 }
--- src/main.rs
+++ src/main.rs
@@ -23,18 +23,63 @@ async fn handle_client(mut stream: TcpStream) -> Result<(), RedisErrors> {
     println!("accepted new connection");
     loop {
         match stream.read(&mut buffer).await {
+            Ok(0) => {
+                // println!("Client disconnected");
+                return Ok(());
+            },
             Ok(n) => {
-                // println!("Read {} bytes!", n);
-                let recv_msg = String::from_utf8_lossy(&buffer[..n]);
-                // println!("Msg: {}", recv_msg);
+                let _recv_msg = String::from_utf8_lossy(&buffer[..n]);
+                // println!("{}", _recv_msg);
+                // println!("{:?}",buffer);
+                let cmds = parse_recv_bytes(&buffer[..n]).await?;
+                // println!("{:?}", cmds[1]);
+                if cmds[0] == String::from("ping") {
+                    stream.write_all(b"+PONG\r\n").await?;
+                }
+                else if cmds[0] == String::from("echo") {
+                    stream.write_all(format!("+{}\r\n",cmds[1]).as_bytes()).await?;
+                }
             },
             Err(e) => {
                 eprintln!("Read error: {}", e);
             }
         }
-
-        stream.write_all(b"+PONG\r\n").await?;
     }
     // Ok(())
+}
+
+async fn parse_recv_bytes(buffer: &[u8]) -> Result<Vec<String>, RedisErrors>{
+    
+    // 1. parse the first line that tells how many args were passed
+    let mut ind = 0;
+    while ind < buffer.len() {
+        if buffer[ind] == 13 && buffer[ind + 1] == 10 {
+            break;
+        }
+        ind+=1;
+    }
+
+    let _num_args = String::from_utf8_lossy(&buffer[1..ind]);
+    
+    ind += 2;
+    let mut prev_ind = ind;
+    
+    let mut cmds = vec![];
+    // 2. parse the remaining content
+    while ind < buffer.len() - 1 {
+        if buffer[ind] == 13 && buffer[ind + 1] == 10 {
+            let arg_size = String::from_utf8_lossy(&buffer[prev_ind+1..ind])
+                            .parse::<usize>()?;
+            let cmd_arg = String::from_utf8_lossy(&buffer[(ind+2)..(ind+2+arg_size)]);
+            cmds.push(cmd_arg.to_lowercase());
+            // println!("val ->{}", String::from_utf8_lossy(&buffer[(ind+2)..(ind+2+arg_size)]));            
+            ind = ind + 2 + arg_size ;
+            prev_ind = ind + 2;
+        }
+        ind += 1;
+    }
+    // println!("{:?}", cmds);
+
+    Ok(cmds)
 }
 
