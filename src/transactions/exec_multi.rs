552f75bc4ef8e0f1c56c2ca300b8ee2cf14b37ce
Abhilash <abhilashmendhe@gmail.com>
2026-04-23 15:05:22 +0530

    codecrafters submit [skip ci]

--- src/transactions/exec_multi.rs
+++ src/transactions/exec_multi.rs
@@ -37,7 +37,7 @@ pub async fn exec_multi(
                     Err(_err) => {
                         // println!("{:?}",_err);
                         commands_transac.clear();
-                        let _ = unwatch(sock_addr, connections.clone(), kv_map.clone()).await;
+                        // let _ = unwatch(sock_addr, connections.clone(), kv_map.clone()).await;
                         return Ok("*-1\r\n".to_string());
                     }
                 }
